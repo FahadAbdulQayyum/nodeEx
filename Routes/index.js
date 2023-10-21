514bd47999908e32292609a00c6e4033bd9f1d9f
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2023-10-21 18:05:21 +0500

    html file is now retrievable

---
+++ Routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    // res.send('Bly')
+    res.sendFile('../client')
+    // const pathFile = path.join(__dirname, '../client')
+    // const pathFile = path.join(__dirname, '../public')
+    // res.sendFile(pathFile)
+})
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
-
+const ROUTER = require('./Routes')
+const path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('AsSalam')
-})
+// app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'client')))
+
+app.use('/', ROUTER)
 
 app.listen(3000, () => {
     console.log('Listening at 3000')
