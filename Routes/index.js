4f5310a77b964b716c54ba4462ce1ccfeb297ee8
Fahad Abdul Qayyum <100997736+FahadAbdulQayyum@users.noreply.github.com>
2023-10-29 17:55:22 +0500

    Update index.js

--- Routes/index.js
+++ Routes/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 // path imported here
 const path = require('path');
-// router imported heree
+// router imported here
 const router = express.Router();
 
 // router controller of get method starts here 
